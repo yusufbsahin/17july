d4119abc930d873ed5895da5aee201c248d3dc38
yusufbsahin <74204859+yusufbsahin@users.noreply.github.com>
2024-07-17 16:33:34 +0300

    Add files via upload

---
+++ 17julyscript.js
@@ -0,0 +1,14 @@
+function changeText() {
+  document.getElementById("ct").innerHTML = "Hello There";
+}
+
+function printPage() {
+  window.print();
+}
+
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+document.getElementById("add").innerHTML = "The value of z is " + z + ".";
